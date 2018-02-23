901e97da242fadc46a3065579b6294bf02730b98
Yael Chavoya <realidadvirtualwm@gmail.com>
2018-02-23 01:14:57 -0600

    Comentarios al main

--- src/fraccionesparser/FraccionesParser.java
+++ src/fraccionesparser/FraccionesParser.java
@@ -3,26 +3,37 @@ package fraccionesparser;
 import java.util.Scanner;
 
 /**
- *
+ * Clase principal del proyecto.
+ * 
  * @author Yael Arturo Chavoya Andalón 14300094
  */
 public class FraccionesParser {
     
+    // Lector de consola
     static Scanner scanner = new Scanner(System.in);
     
     /**
-     * @param args the command line arguments
+     * Función principal.
+     * 
+     * @param args Argumentos de consola
      */
     public static void main(String[] args) {
         String data;
         Parser parser = new Parser();
         
+        // Establecer línea nueva como delimitador
         scanner.useDelimiter("\n");
         
+        //Ciclo principal
         do {
+            // Lectura de datos
             System.out.println("Ingrese operacion en texto> ");
-            data = scanner.next(); 
-            System.out.println(parser.solve(data) + "\n");      
+            data = scanner.next();
+            
+            // Resolver e imprimir
+            if(!data.equals(""))
+                System.out.println(parser.solve(data) + "\n");  
+            
         }while(!data.equals(""));
     }
     
